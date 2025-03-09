c8e8f2e4edb97af564ba13aa38d70716cbf867a2
Brendabaia <br3nd4lima@gmail.com>
2025-03-09 19:24:20 -0300

    criei desafios

---
+++ desafios/d001/script.js
@@ -0,0 +1,10 @@
+function clicar(a){
+    if (a === 1) {
+        alert('Você clicou no 1º botão!')
+    }  else if (a === 2){
+        alert('Você clicou no 2º botão!')
+    } else{
+        alert('Você clicou no 3º botão!')
+    }
+
+}
