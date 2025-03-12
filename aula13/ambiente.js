76cbd453327ae775f402cf167505496efdd315a1
Brendabaia <br3nd4lima@gmail.com>
2025-03-12 12:14:51 -0300

    aula 13 criada

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+ var c = 1
+  do {
+    console.log('Tudo bem?') //repetição final
+    c++
+ } while ( c <= 6)
+
+ /* var c = 1
+ while ( c <= 6) {
+    console.log('Tudo bem?') //repetição inicio
+    c++
+ }*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
