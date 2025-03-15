0ee65716c4bf18f6f14535922ce2a75e88951842
Brendabaia <br3nd4lima@gmail.com>
2025-03-14 23:49:07 -0300

    atualização ex017

--- aula14/ex017/script.js
+++ aula14/ex017/script.js
@@ -5,13 +5,19 @@ function clicar(){
     if(nu.value.length == 0){
         alert('Por favor digite um numero!')
     } else {
-        let n = Number(nu.value)
-        let c = 1
-        while(c <= 10 ){
+         let n = Number(nu.value)
+         tab.innerHTML = ''
+        /*let c = 1
+       while(c <= 10 ){
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             tab.appendChild(item)
             c++
-        }
+        }*/
+       for(let c = 1; c <= 10; c++){
+        let item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}` 
+        tab.appendChild(item)
+       }
     }
 }
